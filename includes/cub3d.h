9ef9a864b452adbfd020c46af57f07088550b563
Theo Deville <tdeville@z3r4p1.42lyon.fr>
2022-10-20 11:26:16 +0200

    init header

--- includes/cub3d.h
+++ includes/cub3d.h
@@ -6,7 +6,7 @@
 /*   By: tdeville <tdeville@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/06 10:42:34 by tdeville          #+#    #+#             */
-/*   Updated: 2022/10/06 14:55:58 by tdeville         ###   ########lyon.fr   */
+/*   Updated: 2022/10/20 11:26:02 by tdeville         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,33 +16,5 @@
 # include "libft.h"
 # include "mlx.h"
 # include <stdio.h>
-
-typedef struct  s_char t_char;
-typedef struct  s_mlx_data t_mlx_data;
-
-struct  t_char
-{
-	int		px;
-	int		py;
-};
-
-struct  s_mlx_data
-{
-	void            *mlx_win;
-	void            *mlx_ptr;
-	char            **matrix;
-	int				init;
-	struct t_char   char_pos;
-};
-
-
-// INIT
-void	init_mlx(t_mlx_data *data);
-
-// DRAW
-void    draw_map(t_mlx_data *data);
-
-// TESTS
-char    **init_matrix_test(char **matrix);
 
 #endif
